1eee66e4376c41831dabe57d18422479a7545a04
Fredj Kharroubi <105554676+fredjkhar@users.noreply.github.com>
2024-04-08 14:45:04 -0400

    add elk version of the python script (#11)

--- sysmon/powershell_sysmon.ps1
+++ sysmon/powershell_sysmon.ps1
@@ -1,3 +1,12 @@
+# PowerShell Script to Extract and Save Sysmon Network Events
+# This script is designed to extract Sysmon network connection events (Event ID 3) related to a specified list of important processes. 
+# It aims to assist in detecting potential beaconing activities by malicious software by focusing on network connections initiated by processes such as svchost.exe, lsass.exe, explorer.exe, among others. 
+# The script queries the Sysmon operational log for events within the last few minutes, filters these events based on the defined process names, and then constructs an XML document containing the filtered events. 
+# Finally, the script saves the constructed XML document to a specified file path, providing a focused dataset for further analysis or investigation into network activities related to the specified processes. 
+# Adjust the list of processes and the time range as needed to tailor the script to specific monitoring requirements.
+
+# NOTE THAT THIS SCRIPT IS FOR TESTING PURPOSES
+
 # Define the log name and the time range for the events
 $logName = "Microsoft-Windows-Sysmon/Operational"
 $startTime = (Get-Date).AddHours(-0.1) # Modify this as needed
@@ -47,5 +56,5 @@ foreach ($event in $events) {
 $xmlOutput += "</Events>"
 
 # Save to XML to a file
-$xmlOutput | Out-File -FilePath "D:\Project Capstone\Scripts\NetworkEventsPastHour.xml" -Encoding UTF8
-Write-Host "XML file has been saved to D:\Project Capstone\Scripts\NetworkEventsPastHour.xml"
+$xmlOutput | Out-File -FilePath "Path\to\NetworkEventsPastHour.xml" -Encoding UTF8
+Write-Host "XML file has been saved."
