69e536671d200b78ec3bd2682b14ab2852b6cccd
fredjkhar <fkhar048@uottawa.ca>
2024-11-15 17:36:29 -0500

    Connect elastic with backend

--- backend/backend/api/views/fetch.py
+++ backend/backend/api/views/fetch.py
@@ -5,34 +5,27 @@ def fetch_data_from_elasticsearch():
     try:
         es = Elasticsearch([{'host': '34.67.212.1', 'port': 9200, 'scheme': 'http'}])
 
-        # Elasticsearch query to fetch EventID 3 documents
         query = {
+            "size": 10000,
             "query": {
-                "match": {
-                    "EventID": 3
+                "range": {
+                "@timestamp": {
+                    "gte": "now-24h/h",
+                    "lt": "now/h"
                 }
-            },
-            "size": 10000,  # Adjust the size according to your data volume
+                }
+            }
         }
 
-        res = es.search(index="sysmon-events", body=query, scroll='2m')
-        scroll_id = res['_scroll_id']
-        total_hits = res['hits']['total']['value']
+        # Execute the query
+        result = es.search(index='.ds-winlogbeat-*', body=query)
 
-        all_hits = res['hits']['hits']
-
-        # Scroll through the data if more than one batch
-        while len(res['hits']['hits']):
-            res = es.scroll(scroll_id=scroll_id, scroll='2m')
-            all_hits.extend(res['hits']['hits'])
-            if len(all_hits) >= total_hits:
-                break
-
-        # Extract the _source field from each hit
-        data = [hit["_source"] for hit in all_hits]
+        # Extract data from the result
+        hits = result['hits']['hits']
+        df = pd.DataFrame([hit['_source'] for hit in hits])
 
         # Create a DataFrame
-        return pd.DataFrame(data)
+        return df
     except Exception as e:
         # Handle exceptions (e.g., connection errors)
         print(f"Error fetching data from Elasticsearch: {e}")
