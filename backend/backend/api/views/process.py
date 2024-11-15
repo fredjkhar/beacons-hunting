35919a2413cf8ab5a1017545421c01514cbd727b
fredjkhar <fkhar048@uottawa.ca>
2024-11-15 17:36:29 -0500

    Connect elastic with backend

--- backend/backend/api/views/process.py
+++ backend/backend/api/views/process.py
@@ -3,19 +3,30 @@ from dateutil.parser import parse
 from .score import compute_bowleys_skewness, compute_mad_score, compute_connection_count_score, compute_combined_score
 
 def process_data(df_original):
-    df = df_original[df_original["EventID"] == 3].copy()
+    df = df_original.copy()
+    df_destination = df['destination'].apply(pd.Series)
+    df_source = df['source'].apply(pd.Series)
+    df_process = df['process'].apply(pd.Series)
 
-    target_columns = ['SystemTime', 'SourceIp', 'DestinationIp']
-    df = df[target_columns].dropna()
+    # Rename the columns to avoid name conflicts
+    df_destination = df_destination.rename(columns=lambda x: 'destination.' + str(x))
+    df_source = df_source.rename(columns=lambda x: 'source.' + str(x))
+    df_process = df_process.rename(columns=lambda x: 'process.' + str(x))
 
-    if df.empty:
-        return pd.DataFrame()
+    # Concatenate the expanded columns back to the original DataFrame
+    df = pd.concat([df.drop(['destination', 'source'], axis=1), df_destination, df_source, df_process], axis=1)
 
+    # Now, filter the DataFrame to include only the target columns
+    target_columns = ["destination.ip", "source.ip", "@timestamp", "process.name", "process.executable"]
+    df = df[target_columns]
+
+    # Drop rows with missing (NaN) values
+    df = df.dropna()
     df = (
-        df.groupby(["SourceIp", "DestinationIp"])
+        df.groupby(["source.ip", "destination.ip"])
         .agg(
-            TotalConnections=pd.NamedAgg(column="SystemTime", aggfunc="count"),
-            ConnectionTimes=pd.NamedAgg(column="SystemTime", aggfunc=list),
+            TotalConnections=pd.NamedAgg(column="@timestamp", aggfunc="count"),
+            ConnectionTimes=pd.NamedAgg(column="@timestamp", aggfunc=list),
         )
         .reset_index()
     )
--- backend/backend/api/views/views.py
+++ backend/backend/api/views/views.py
@@ -8,11 +8,12 @@ from .fetch_csv import fetch_data_from_csv
 
 def my_view(request):
     # Currently using a local csv file for testing until the issue with the VM gets resolved
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    csv_file_path = os.path.join(BASE_DIR, 'data', 'data.csv')
-    df_original = fetch_data_from_csv(csv_file_path)
+    #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #csv_file_path = os.path.join(BASE_DIR, 'data', 'data.csv')
+    #df_original = fetch_data_from_csv(csv_file_path)
 
-    #df_original = fetch_data_from_elasticsearch()
+    df_original = fetch_data_from_elasticsearch()
+    print(len(df_original))
 
     if df_original.empty:
         return JsonResponse({'error': 'No data fetched from Elasticsearch.'}, status=500)
