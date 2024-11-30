ec49eba565d942632b43b125d680b70fd88304df
https://git.ongarde.net/NoahT <nthom039@uottawa.ca>
2024-11-29 22:02:29 -0500

    Modified backend / api to work with selected start and end time
    api/get works the same as before, but now you can also select a start and end date/time
    Ex: api/get/?start_time=2024-11-28T00:00:00&end_time=2024-11-28T23:59:59

--- backend/backend/api/views/fetch.py
+++ backend/backend/api/views/fetch.py
@@ -1,18 +1,48 @@
 import pandas as pd
 from elasticsearch import Elasticsearch
 
-def fetch_data_from_elasticsearch():
+def fetch_data_from_elasticsearch(start_time, end_time):
     try:
-        es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200, 'scheme': 'http'}])
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 
         query = {
             "size": 10000,
             "query": {
                 "range": {
-                "@timestamp": {
-                    "gte": "now-24h/h",
-                    "lt": "now/h"
+                    "@timestamp": {
+                        "gte": start_time,
+                        "lt": end_time
+                    }
                 }
+            }
+        }
+
+        # Execute the query
+        result = es.search(index='.ds-winlogbeat-*', body=query)
+
+        # Extract data from the result
+        hits = result['hits']['hits']
+        df = pd.DataFrame([hit['_source'] for hit in hits])
+
+        # Create a DataFrame
+        return df
+    except Exception as e:
+        # Handle exceptions (e.g., connection errors)
+        print(f"Error fetching data from Elasticsearch: {e}")
+        return pd.DataFrame()
+
+def fetch_data_from_elasticsearch_last_24h():
+    try:
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+
+        query = {
+            "size": 10000,
+            "query": {
+                "range": {
+                    "@timestamp": {
+                        "gte": "now-24h/h",
+                        "lt": "now/h"
+                    }
                 }
             }
         }
